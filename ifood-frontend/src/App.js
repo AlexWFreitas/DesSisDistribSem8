651cd5cccd669c8d3f13ce942e02af8b0a938f73
Alex Wong <alex.wong@orangetesting.com>
2021-10-19 21:24:10 -0300

    Implementando uma página com React para o front-end.

---
+++ ifood-frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="pagina-principal">
+      <div className="conteudo">
+        <div className="preco">
+          Pratos a partir de R$159,90.
+        </div>
+        <h1 className="slogan">COMER MUDA TUDO</h1>
+        <a className="botao"> Verificar </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
