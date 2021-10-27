820ba2b2e3ebe56323dffd9ca08e4caa5453c429
Alex Wong <alex.wong@orangetesting.com>
2021-10-26 21:57:24 -0300

    Added Axios and API Requests

--- ifood-frontend/src/pages/Dashboard.js
+++ ifood-frontend/src/pages/Dashboard.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import './styles/Style.css';
+import React, { useEffect, useState } from "react";
+import api from "../services/api";
+import "./styles/Style.css";
 
 function Dashboard() {
-	return (
-		<h1> Página criada (existe!!) </h1>
-	)
+  const [restaurantes, setRestaurantes] = useState([]);
+  useEffect(() => {
+    api.get("api/restaurantes/retornaTodos").then((resposta) => {
+		setRestaurantes(resposta.data)
+    });
+  }, []);
+
+  return (
+    <div className="row">
+		{
+			restaurantes.map(restaurante => {				
+				return (
+					<div className="card" key={restaurante.id}>
+						<h2> {restaurante.nome} </h2>
+						<p> {restaurante.tipo} </p>
+						<span className="text-muted"> {restaurante.local} </span>
+					</div>
+				)	
+			})
+		}			      	
+    </div>
+  );
 }
 
 export default Dashboard;
